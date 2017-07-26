174f2bea3aa455370d5529f28a54a7b84a448d46
Srinivaslancius <srinivas@lanciussolutions.com>
2017-07-26 14:43:17 +0530

    dashboard changes in admibn panel

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,205 +3,182 @@
 <?php include_once 'side_navigation.php';?>
 
             <main class="mn-inner">
-                <div class="row">                    
+
+                <div>
+                    <div class="row no-m-t no-m-b">
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    
+                                </div>
+                                <span class="card-title">Orders</span>
+                                <span class="stats-counter">$<span class="counter">48190</span><small>This week</small></span>
+                            </div>
+                            <div id="sparkline-bar"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    <ul>
+                                        <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                                    </ul>
+                                </div>
+                                <span class="card-title">Orders</span>
+                                <span class="stats-counter"><span class="counter">83710</span><small>This month</small></span>
+                            </div>
+                            <div id="sparkline-line"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    <ul>
+                                        <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                                    </ul>
+                                </div>
+                                <span class="card-title">Orders</span>
+                                <span class="stats-counter"><span class="counter">83710</span><small>This Year</small></span>
+                            </div>
+                            <div id="sparkline-line"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    <ul>
+                                        <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                                    </ul>
+                                </div>
+                                <span class="card-title">Orders</span>
+                                <span class="stats-counter"><span class="counter">83710</span><small>Total</small></span>
+                            </div>
+                            <div id="sparkline-line"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    <ul>
+                                        <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                                    </ul>
+                                </div>
+                                <span class="card-title">Admin Users</span>
+                                <span class="stats-counter"><span class="counter">83710</span></span>
+                            </div>
+                            <div id="sparkline-line"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">                                
+                                <span class="card-title">Customers</span>
+                                <span class="stats-counter">$<span class="counter">48190</span><small>This week</small></span>
+                            </div>
+                            <div id="sparkline-bar"></div>
+                        </div>
+                    </div>
+                    <div class="col s12 m12 l3">
+                        <div class="card stats-card">
+                            <div class="card-content">
+                                <div class="card-options">
+                                    <ul>
+                                        <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                                    </ul>
+                                </div>
+                                <span class="card-title">Products</span>
+                                <span class="stats-counter"><span class="counter">83710</span></span>
+                            </div>
+                            <div id="sparkline-line"></div>
+                        </div>
+                    </div>
+                
                    
-                    <div class="col s12 m6 l6">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Line Chart<small>Chart.js</small></span>
-                                <div>
-                                    <canvas id="chart1" width="400" height="270"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Radar Chart<small>Chart.js</small></span>
-                                <div>
-                                    <canvas id="chart3" width="400" height="200"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m6 l6">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Bar Chart<small>Chart.js</small></span>
-                                <div>
-                                    <canvas id="chart2" width="400" height="270"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Pie Chart<small>Chart.js</small></span>
-                                <div>
-                                    <canvas id="chart4" width="400" height="200"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <div class="row">
-                    <div class="col s12 m12 l12">
-                        <div class="card card-transparent">
-                            <div class="card-content">
-                                <span class="card-title black-text">NVD3</span>
-                                <p>This project is an attempt to build re-usable charts and chart components for d3.js without taking away the power that d3.js gives you. This is a very young collection of components, with the goal of keeping these components very customizeable, staying away from your standard cookie cutter solutions.</p>
-                                <br>
-                                <a href="http://nvd3.org/index.html" target="blank" class="secondary-link">View the official documentation</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m6 l6">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Line Chart<small>NVD3</small></span>
-                                <div id="nvd1"><svg></svg></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Group / Stacked Bar Chart<small>NVD3</small></span>
-                                <div id="nvd3"><svg></svg></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m6 l6">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Cumulative Line Chart<small>NVD3</small></span>
-                                <div id="nvd2"><svg></svg></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Stacked Area Chart<small>NVD3</small></span>
-                                <div id="nvd4"><svg></svg></div>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
-                <div class="row">
-                    <div class="col s12 m12 l12">
-                        <div class="card card-transparent">
-                            <div class="card-content">
-                                <span class="card-title black-text">Flot</span>
-                                <p>Flot is a pure JavaScript plotting library for jQuery, with a focus on simple usage, attractive looks and interactive features.</p>
-                                <br>
-                                <a href="http://www.flotcharts.org/" target="blank" class="secondary-link">View the official documentation</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m6 l6">
+               
+                <div class="row">                    
+                    <div class="col s12 m6 l6">                        
                         <div class="card">
                             <div class="card-content">
-                                <span class="card-title">Bar Chart<small>Flot</small></span>
-                                <div id="flot1"></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Pie Chart<small>Flot</small></span>
+                                <span class="card-title">Chart For Orders</span>
                                 <div id="flot3"></div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col s12 m6 l6">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Line Chart<small>Flot</small></span>
-                                <div id="flot2"></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Live Chart<small>Flot</small></span>
-                                <div id="flot4"></div>
-                            </div>
-                        </div>
-                    </div>
+                    </div>                 
                 </div>
-                <div class="row">
-                    <div class="col s12 m12 l12">
-                        <div class="card card-transparent">
-                            <div class="card-content">
-                                <span class="card-title black-text">Sparkline</span>
-                                <p>This jQuery plugin generates sparklines (small inline charts) directly in the browser using data supplied either inline in the HTML, or via javascript.</p>
-                                <br>
-                                <a href="http://omnipotent.net/jquery.sparkline/#s-docs" target="blank" class="secondary-link">View the official documentation</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m4 l4">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Line Chart<small>Flot</small></span>
-                                <div id="sparkline1"></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Custom Line Chart<small>Flot</small></span>
-                                <div id="sparkline4"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m4 l4">
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Bar Chart<small>Flot</small></span>
-                                <div id="sparkline2"></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Mouse Speed Chart Example<small>Flot</small></span>
-                                <div id="sparkline5"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col s12 m4 l4">
-                        
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Pie Chart<small>Flot</small></span>
-                                <div id="sparkline3"></div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <span class="card-title">Composite bar Chart<small>Flot</small></span>
-                                <div id="sparkline6"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </main>
-           
-        </div>
-        <div id="chartjs-tooltip"></div>
-        <div class="left-sidebar-hover"></div>
         
-        <!-- Javascripts -->
-        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
-        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
-        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
-        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-        <script src="assets/plugins/chart.js/chart.min.js"></script>
-        <script src="assets/plugins/d3/d3.min.js"></script>
+
+        <?php
+            $sql= "SELECT * FROM orders";
+            $result = $conn->query($sql);
+            //$row = $result->fetch_assoc();
+            //echo "<pre>"; print_r($row);     die;
+        ?>
+        <?php include_once 'footer.php'; ?>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                
+                var flot3 = function () {
+                    var data = [{
+                        label: "1",
+                        data: 42,
+                        color: "#3366CC",
+                    }, {
+                        label: "2",
+                        data: 23,
+                        color: "#ff9800",
+                    }, {
+                        label: "3",
+                        data: 35,
+                        color: "#82b1ff",
+                    }];
+                    var options = {
+                        series: {
+                            pie: {
+                                show: true
+                            }
+                        },
+                        legend: {
+                            labelFormatter: function(label, series){
+                                return '<span class="pie-chart-legend">'+label+'</span>';
+                            }
+                        },
+                        grid: {
+                            hoverable: true
+                        },
+                        tooltip: true,
+                        tooltipOpts: {
+                            content: "%p.0%, %s",
+                            shifts: {
+                                x: 20,
+                                y: 0
+                            },
+                            defaultTheme: false
+                        }
+                    };
+                    $.plot($("#flot3"), data, options);
+                };
+
+                flot3();
+               
+            });
+        </script>
+        
+        <!-- Javascripts -->        
         <script src="assets/plugins/nvd3/nv.d3.min.js"></script>
         <script src="assets/plugins/flot/jquery.flot.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.symbol.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.resize.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>             
         <script src="assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.pie.min.js"></script>
-        <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-        <script src="assets/js/alpha.min.js"></script>
-        <script src="assets/js/pages/charts.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.pie.min.js"></script>      
         
-    </body>
-
-<!-- Mirrored from steelcoders.com/alpha/v1.2/charts.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 04 Jul 2017 06:56:27 GMT -->
-</html>
+  

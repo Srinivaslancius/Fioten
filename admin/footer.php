11257b27a8ab9b3ece783d3841d8f541b7485272
srinivaslancius <srinivas@lanciussolutions.com>
2017-08-07 17:09:30 +0530

    added some script for validation

--- admin/footer.php
+++ admin/footer.php
@@ -25,6 +25,22 @@
             var output = document.getElementById('output');
             output.src = URL.createObjectURL(event.target.files[0]);
           };
+          $("form").submit(function(e) {
+
+            var ref = $(this).find("[required]");
+
+            $(ref).each(function(){
+                if ( $(this).val() == '' )
+                {
+                    alert("Required field should not be blank.");
+
+                    $(this).focus();
+
+                    e.preventDefault();
+                    return false;
+                }
+            });  return true;
+        });
         </script>
     </body>
 </html>
